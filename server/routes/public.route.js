4027233e9fe2160d0b7798018e42f9a9ea5bbf4f
kaushik tadhani <kaushiktadhani@gmail.com>
2018-01-28 19:18:48 +0530

    access log file api added

--- server/routes/public.route.js
+++ server/routes/public.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './auth.route';
 import countryRoutes from './country.route';
+import FileSystem from 'fs';
 
 const router = express.Router();
 
@@ -8,6 +9,17 @@ const router = express.Router();
 router.get('/health-check', (req, res) =>
   res.send('OK')
 );
+
+router.get('/access-log', (req, res) => {
+  let filePath=`${__basedir}\\access-log.txt`;
+  FileSystem.readFile(filePath, function (err, data) {
+    if (err) {
+      res.send(err);
+    }
+    res.setHeader('Content-type', 'text/plain');
+    res.send(data);
+  });
+});
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
